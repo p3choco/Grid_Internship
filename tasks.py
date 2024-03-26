9e30ee67754240501cd7cdb4632f1fd7aba85675
p3choco <1piotrbednarski1@gmail.com>
2024-03-26 13:33:03 +0100

    primal setup done

---
+++ tasks.py
@@ -0,0 +1,17 @@
+"""
+1-Having those 2 lists, print the list which contains elements, which are common for those 2 lists:
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].
+
+2-Having dictionary:
+{1: 2, 3: 4, 4: 3, 2: 1, 0: 0}
+Sort it ask and desc based on values;
+
+3-Write a code, which will concat 2 dictionary into the one and sum up values for common keys.
+dict1 = {1:10, 2:20}; disc2 = {1:20, 2:30}
+
+4-Find top 3 dict keys which have the highest values.
+{'a':500, 'b':5874, 'c': 560,'d':400, 'e':5874, 'f': 20}
+"""
+
+
